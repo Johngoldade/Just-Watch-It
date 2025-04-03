6e0a6e902dcdfdcead92ff2fcc2582fe304034da
John Goldade (Work) <goldade.john@gmail.com>
2025-04-02 20:55:42 -0500

    Built main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,48 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+
 import App from './App.tsx'
+import HomePage from './pages/Home.tsx'
+import MovieDB from './pages/MovieDB.tsx'
+import Groups from './pages/Groups.tsx'
+import MyMovies from './pages/MyMovies.tsx'
+import ErrorPage from './pages/Error.tsx'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <HomePage /> 
+      },
+      {
+        path: '/Movies',
+        element: <MovieDB /> 
+      },
+      {
+        path: '/Groups',
+        element: <Groups />
+      },
+      {
+        path: '/Mymovies',
+        element: <MyMovies />
+      }
+    ]
+  }
+])
+
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
