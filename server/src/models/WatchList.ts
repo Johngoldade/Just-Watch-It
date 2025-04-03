6ee45c58036333d2a10afa4252a5d19dd76f1f42
Jpeterson <peterson.jacob13@protonmail.com>
2025-04-03 18:55:33 -0500

    added return to WatchList.ts

--- server/src/models/WatchList.ts
+++ server/src/models/WatchList.ts
@@ -10,6 +10,8 @@ import {
 
 import { Rating } from './Rating.js'
 
+import { Movie } from './Movie.js'
+
 export class WatchList extends Model<
     InferAttributes<WatchList>,
     InferCreationAttributes<WatchList>
@@ -18,6 +20,38 @@ export class WatchList extends Model<
     declare priority: number;
     declare watched: boolean;
     declare rating: ForeignKey<Rating['id']>;
-    declare movieId: ForeignKey<Movie['id']>;
+    declare movieId: ForeignKey<Movie['imdbID']>;
     declare groupRating: number;
 }
+
+export function WatchListFacotry(sequelize: Sequelize): typeof WatchList {
+    WatchList.init(
+        {
+            id: {
+                type: DataTypes.INTEGER,
+                autoIncrement: true,
+                primaryKey: true,
+            },
+            priority: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+            },
+            watched: {
+                type: DataTypes.BOOLEAN,
+                allowNull: false,
+                defaultValue: false,
+            },
+            groupRating: {
+                type: DataTypes.INTEGER,
+                allowNull: true,
+            }
+        },
+        {
+            sequelize,
+            timestamps: false,
+            underscored: true,
+            modelName: 'watch lists'
+        })
+
+    return WatchList
+};
