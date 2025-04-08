e77b28df510834bce89f9eb4368f2eb0cb74a661
John Goldade (Work) <goldade.john@gmail.com>
2025-04-07 20:19:26 -0500

    Added authentication middleware and used it to get user id for favorites routes

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,9 +1,10 @@
 import dbRouter from './DB API/index.js';
 import express from 'express';
+import authenticationToken from '../middleware/authentication.js';
 
 const router = express.Router();
 
-router.use('/db', dbRouter);
+router.use('/db', authenticationToken, dbRouter);
 
 
 export default router;
