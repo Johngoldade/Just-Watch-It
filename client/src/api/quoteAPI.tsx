97651e5c1f676876bde99167d6821cbbcaa4583f
Brett Lintgen <blintgen@gmail.com>
2025-04-09 20:55:50 -0500

    added everything to put a quote on the homepage

---
+++ client/src/api/quoteAPI.tsx
@@ -0,0 +1,15 @@
+import { Quote } from "../interfaces/quote"
+
+export const fetchQuote = async (): Promise<Quote | null> => {
+    try {
+        const response = await fetch('/quote')
+        if (!response.ok) {
+            throw new Error('Invalid API response')
+        }
+        const data: Quote = await response.json()
+        return data
+    } catch (error) {
+        console.error(`Error retrieving data... see this error => ${error}`)
+        return null
+    }
+}
